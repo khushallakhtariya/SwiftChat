21bb9fa7f0e7ff436da30e460d6498efee9652f7
unknown <you@example.com>
2025-05-29 10:11:11 +0530

    fix thet hooks

--- src/components/History.js
+++ src/components/History.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import swal from "sweetalert";
 
 const History = ({ contactHistory, setContactHistory }) => {
--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { saveHistory } from "../utils/contactUtils";
 
 const MainForm = ({ setContactHistory, setYourContacts }) => {
